54bdcf0a54cbc06cd2d062dc28e42c65ad41196b
Hector Valdes <contacto@hectorvaldesm.com>
2022-07-29 23:58:08 -0400

    Project UpperLower first version 1.0

---
+++ app.js
@@ -0,0 +1,30 @@
+let txtArea = document.querySelector(".txt-to-trans");
+let buttons = document.querySelector(".sect__buttons");
+let replaceText = "";
+
+buttons.addEventListener("click", (e) => {
+  e.stopPropagation();
+
+//minusculas
+  if (e.target.classList.contains("btn-min")) {
+    replaceText = txtArea.value;
+    if(replaceText.length > 0){
+        let str = replaceText.toLocaleLowerCase();
+        txtArea.value = str;
+        txtArea.focus();
+        txtArea.select();
+    }
+  }
+  //mayusculas
+  if (e.target.classList.contains("btn-may")) {
+    replaceText = txtArea.value;
+        if(replaceText.length > 0){
+            let str = replaceText.toLocaleUpperCase();
+            txtArea.value = str;
+            txtArea.focus();
+            txtArea.select();
+        }
+  }
+  if(e.target.classList.contains("btn-clear")){
+  }
+});
