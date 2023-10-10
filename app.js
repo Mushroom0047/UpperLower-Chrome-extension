e94a707def5bd1b09ebd1b0563bfcf6c21cc617b
Mushroom <hectorvaldesm47@gmail.com>
2023-10-10 10:54:15 -0300

    Modify the manifest with the icons

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-// "default_icon": {
-//   "16": "/images/get_started16.png",
-//   "32": "/images/get_started32.png",
-//   "48": "/images/get_started48.png",
-//   "128": "/images/get_started128.png"
-// }
 let txtArea = document.querySelector(".txt-to-trans");
 let buttons = document.querySelector(".sect__buttons");
 let replaceText = "";
