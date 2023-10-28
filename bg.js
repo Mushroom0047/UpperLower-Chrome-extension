40e65ce08ae6c4001b7fec879b9f598a34d9783c
Hector Valdes <contacto@hectorvaldesm.com>
2023-10-27 22:33:25 -0300

    The bd.js file is changed to correct duplicate id errors

--- bg.js
+++ bg.js
@@ -1,9 +1,64 @@
 chrome.runtime.setUninstallURL('https://forms.gle/RrjQJ5BmxHzmXzrY8');
-chrome.contextMenus.create({
-    id: "myContextMenu",
-    title: "UpperLower | Text converter",
-    contexts: ["all"],    
+
+chrome.runtime.onInstalled.addListener(() => {
+  let contextMenuId;  // Declarar la variable antes del bloque if
+
+  chrome.storage.local.get('contextMenuId', (result) => {
+    contextMenuId = result.contextMenuId;
+
+    if (!contextMenuId) {
+      // Generar un nuevo ID
+      contextMenuId = 'myContextMenu_' + Math.random().toString(36).substring(7);
+
+      // Guardar el ID en el almacenamiento local
+      chrome.storage.local.set({ contextMenuId }, () => {
+        // Crear el elemento de menú contextual
+        createContextMenu(contextMenuId);
+      });
+    } else {
+      // Crear el elemento de menú contextual con el ID existente
+      createContextMenu(contextMenuId);
+    }
   });
+});
+
+function createContextMenu(contextMenuId) {
+  chrome.contextMenus.create({
+    id: contextMenuId,
+    title: "UpperLower | Text converter",
+    contexts: ["all"],
+  });
+
+  chrome.contextMenus.create({
+    id: "option1",
+    title: "lowercase",
+    contexts: ["all"],
+    parentId: contextMenuId,
+  });
+
+  chrome.contextMenus.create({
+    id: "option2",
+    title: "UPPERCASE",
+    contexts: ["all"],
+    parentId: contextMenuId,
+  });
+
+  chrome.contextMenus.create({
+    id: "option3",
+    title: "Capitalize",
+    contexts: ["all"],
+    parentId: contextMenuId,
+  });
+
+  chrome.contextMenus.create({
+    id: "option4",
+    title: "Capitalize All",
+    contexts: ["all"],
+    parentId: contextMenuId,
+  });
+}
+
+  
   chrome.contextMenus.onClicked.addListener(function (info, tab) {
     if (info.menuItemId === "option1" || info.menuItemId === "option2" || info.menuItemId === "option3" || info.menuItemId === "option4") {
       // Enviar un mensaje al script de contenido para que realice la operación en el DOM de la página
@@ -42,32 +97,4 @@ chrome.contextMenus.create({
         args: [info.selectionText, info.menuItemId]
       });
     }
-  });
-
-  chrome.contextMenus.create({
-    id: "option1",
-    title: "lowercase",
-    contexts: ["all"],
-    parentId: "myContextMenu",
-  });
-  
-  chrome.contextMenus.create({
-    id: "option2",
-    title: "UPPERCASE",
-    contexts: ["all"],
-    parentId: "myContextMenu",
-  });
-  
-  chrome.contextMenus.create({
-    id: "option3",
-    title: "Capitalize",
-    contexts: ["all"],
-    parentId: "myContextMenu",
-  });
-  
-  chrome.contextMenus.create({
-    id: "option4",
-    title: "Capitalize All",
-    contexts: ["all"],
-    parentId: "myContextMenu",
   });
